e7018ab1affd07a258061df29899f6260511fdcb
Diogo Souza <diogosouza2@gmail.com>
2015-12-19 22:02:30 +0000

    Add delete, fix control and view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+  
   def new
     @article = Article.new
   end
@@ -14,11 +16,9 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated."
       redirect_to article_path(@article)
@@ -26,15 +26,23 @@ class ArticlesController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def index
-  
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+    flash[:notice] = "Article successfully deleted"
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
+
   private
+  def set_article
+      @article = Article.find(params[:id])  
+  end
   def article_params
     params.require(:article).permit(:title, :description)
   end
